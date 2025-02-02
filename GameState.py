1a3836cc14c080f717e728ccd05c0c86a5343756
Tony Reynolds <grondak@gmail.com>
2025-02-01 20:48:09 -0500

    Simple demo of offloading

--- GameState.py
+++ GameState.py
@@ -2,6 +2,9 @@
 import csv
 from T5World import T5World
 from T5Lot import *
+from T5Starship import *
+from T5Mail import *
+from T5NPC import *
 
 MAP_FILE = "t5_map.txt"
 
@@ -23,6 +26,7 @@ class GameState:
                     "Zone": row["Zone"],
                     "Coordinates": (int(row["Hex"][:2]), int(row["Hex"][2:])),  # (X, Y)
                     "TradeClassifications": row["Remarks"],
+                    "Importance": row["{Ix}"],
                 }
         return worlds
     
@@ -41,4 +45,31 @@ if __name__ == '__main__':
     print(f'The test lot ID is {test_lot.lot_id} with mass {test_lot.mass} and serial {test_lot.serial}')
     print(f'\tSelling World: {world} trade classifications: {GameState.world_data[world].trade_classifications()} ' +
           f'tech level: {letter_to_tech_level(GameState.world_data[world].UWP()[8:])} // lot value: {test_lot.determine_sale_value_on(world, GameState)}')
-        
+    npc1 = T5NPC('Admiral Miller')
+    npc2 = T5NPC('General Sprange')
+    starship = T5Starship('Paprika', 'Rhylanor')
+    starship.set_course_for('Jae Tellona')
+    mail1 = T5Mail('Rhylanor', 'Jae Tellona', GameState)
+    mail2 = T5Mail('Rhylanor', 'Jae Tellona', GameState)
+    starship.onload_high_passenger(npc1)
+    starship.onload_high_passenger(npc2)
+    starship.onload_mail(mail1)
+    starship.onload_mail(mail2)
+    print(f'\n\n\nStarship {starship.shipName} bound for {starship.destination()}. Contents:')
+    for passenger in starship.highPassengers:
+        print(f'\tPassenger {passenger.characterName}.')
+    for mailItem in starship.get_mail():
+        print(f'\tMail bundle with serial number {starship.get_mail()[mailItem].serial}.')
+    print(f'Starship {starship.shipName} arriving at {starship.destination()}.')
+    starship.location= starship.destination
+    starship.set_course_for('Rhylanor')
+    print(f'Starship {starship.shipName} now bound for {starship.destination()}.')    
+    print('Priority Offload High Passengers!')
+    starship.offload_high_passengers()
+    print(f'\t{starship.shipName} has {len(starship.highPassengers)} high passengers aboard.')
+    print('Priority Offload Mail!')
+    starship.offload_mail()
+    print(f'\t{starship.shipName} has {len(starship.get_mail())} mail bundles in the mail locker.')
+    
+    
+    print("End simulation version 0.2")
